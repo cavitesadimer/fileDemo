b30211d517bdc2ee805d584d96dac5132404789d
Cavit Esad Imer <cavitesadimer@gmail.com>
2023-02-23 14:38:06 +0300

    Dosya islemleri metoda donusturuldu

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+    createFile();
+    getFileInfo();
 
+    }
+    public static void createFile(){
         File file = new File("C:\\Users\\Esad\\Desktop\\dosyaIslemleri.txt");
         try {
             if(file.createNewFile()){
@@ -11,7 +15,16 @@ public class Main {
         } catch (Exception e) {
             System.out.println("Dosya islemleri yapilirken hata olustu!!!");
         }
+    }
+    public static void getFileInfo(){
+        File file = new File("C:\\Users\\Esad\\Desktop\\dosyaIslemleri.txt");
+        if(file.exists()){
+            System.out.println("Dosya adi: "+file.getName());
+            System.out.println("Dosya yolu: "+file.getPath());
+            System.out.println("Dosya okunabiliyor mu?: "+file.canRead());
+            System.out.println("Dosya yazilabiliyor mu?: "+file.canWrite());
+            System.out.println("Dosya boyutu(byte): "+file.length());
 
-
+        }else System.out.println("ERROR!!! Dosya mevcut degil");
     }
 }
